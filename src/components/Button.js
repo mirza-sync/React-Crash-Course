9f262c9ac2816b739135f7872c17c197a7aeda76
mirza-sync <mirzasyahmi123@gmail.com>
2021-08-28 16:59:35 +0800

    Add header

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({ color, text, onClick }) => {
+    return (
+        <button onClick={onClick} style={{background: color}} 
+        className='btn'>
+            {text}
+        </button>
+    )
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = () => {
+const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('clicked')
+    }
+
     return (
-        <header>
-            Task Tracker
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
+
+Header.propTypes = {
+    title: PropTypes.string
+}
+
+// // CSS in JS
+// const headingStyle = {
+//     color: 'blue',
+//     background: 'yellow'
+// }
 
 export default Header
