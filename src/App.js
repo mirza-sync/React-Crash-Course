775977515e98e81feb67db3868c61ad3995e3214
mirza-sync <mirzasyahmi123@gmail.com>
2021-08-28 22:12:19 +0800

    Add tasks and state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import React, { useState } from 'react'
 import Header from "./components/Header";
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Sleep',
+        day: 'Tuesday',
+        reminder: false
+    },
+    {
+        id: 2,
+        text: 'Eat',
+        day: 'Tuesday',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Code',
+        day: 'Tuesday',
+        reminder: true
+    }
+  ])
+
   return (
     <div className="container">
       <Header title="My Task Tracker" />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
