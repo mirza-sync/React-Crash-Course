75e893ac122b60f58210ffca65bebc67dfded025
mirza-sync <mirzasyahmi123@gmail.com>
2021-09-03 23:53:23 +0800

    Make toggle reminder update on server

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ function App() {
     return data
   }
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
   const addTask = async (task) => {
     const res = await fetch('http://localhost:5000/tasks', {
       method: 'POST',
@@ -42,9 +48,20 @@ function App() {
     setTasks(tasks.filter(task => task.id !== id))
   }
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+    const data = await res.json()
+
     setTasks(tasks.map(task => 
-      task.id === id ? {...task, reminder: !task.reminder} : task
+      task.id === id ? {...task, reminder: data.reminder} : task
     ))
   }
 
